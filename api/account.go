0d5b01b2c1be35e37894f43f2a8a4848a509f0a9
nes224 <nes224@hotmail.com>
2024-06-12 15:52:59 +0700

    go using gin

--- api/account.go
+++ api/account.go
@@ -1,36 +1,31 @@
 package api
 
 import (
+	"database/sql"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	request "github.com/techschool/simplebank/api/request"
 	db "github.com/techschool/simplebank/db/sqlc"
 )
-
-type createAccountRequest struct {
-	Owner    string `json:"owner" binding:"required"`    // golang to json
-	Currency string `json:"currency" binding:"required,oneof=USD EUR"` // golang to json
-}
-
 
 func (server *Server) createAccount(ctx *gin.Context) { // server * Server pointer receiver.
 	// ctx *gin.Context -> *Context input -> Basically, when using gin, everything we do inside a handler will involve this context object.
 	// It provides a lot of convenient methods to read input parameters and write out responses.
 	// ShouldBindJSON function to parse the input data from HTTP request body and Gin will validate the output object to make sure
 	// it satisfy the conditions we specified in the binding tag.
-	var req createAccountRequest
+	var req request.CreateAccountRequest
 	if err := ctx.ShouldBindJSON(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
-
 	arg := db.CreateAccountParams{
-		Owner: req.Owner,
+		Owner:    req.Owner,
 		Currency: req.Currency,
-		Balance: 0,
+		Balance:  0,
 	}
-	
-	account, err := server.store.CreateAccount(ctx,arg)
+
+	account, err := server.store.CreateAccount(ctx, arg)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
@@ -39,3 +34,45 @@ func (server *Server) createAccount(ctx *gin.Context) { // server * Server point
 	ctx.JSON(http.StatusOK, account)
 }
 
+func (server *Server) getAccount(ctx *gin.Context) {
+	var req request.GetAccountRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	account, err := server.store.GetAccountForUpdate(ctx, req.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, account)
+}
+
+
+func (server *Server) listAccount(ctx *gin.Context) {
+	var req request.ListAccountRequest
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.ListAccountsParams{
+		Limit: req.PageSize,
+		Offset: (req.PageID - 1) * req.PageSize,
+	}
+
+	accounts, err := server.store.ListAccounts(ctx, arg)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, accounts)
+}
--- main.go
+++ main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/techschool/simplebank/api"
+	db "github.com/techschool/simplebank/db/sqlc"
+	"github.com/techschool/simplebank/util"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config")
+	}
+
+	connPool, err := pgxpool.New(context.Background(), config.DBSource)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	store := db.NewStore(connPool)
+	server := api.NewServer(store)
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
+	
+}
